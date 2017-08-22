0418308ad7ff0ce6204f53a2cc6610bbe21ab990
Edan Leibovitz <Leibo.edan@gmail.com>
2017-08-22 10:44:30 +0300

    Refactoring Models

---
+++ FinalProject/Dal/GeneralDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using EX5.Models;
+
+namespace FinalProject.Dal
+{
+    public class GeneralDbContext : DbContext
+    {
+        public DbSet<User> DBUser { get; set; }
+        public DbSet<Business> DBBusiness { get; set; }
+        public DbSet<Rank> DBRank { get; set; }
+    }
+}
