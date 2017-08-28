32166b42c312431b9deb0351e6f6d80c23b99a33
Tuval Lifshitz <tuval.lif@gmail.com>
2017-08-28 17:21:12 +0300

    changing map location, now can show slideshow in contact

--- FinalProject/Scripts/_references.js
+++ FinalProject/Scripts/_references.js
@@ -4,6 +4,5 @@
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="modernizr-2.6.2.js" />
-/// <reference path="pixastic.js" />
 /// <reference path="respond.js" />
 /// <reference path="script.js" />
